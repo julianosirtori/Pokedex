69a4c062fbf9ff9d055c522bf3ce75eb4fa403d2
Juliano Sirtori <julianosirtori@gmail.com>
2020-04-26 11:26:35 -0300

    Add lib for work withs svg

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
 
-import { Container } from './styles';
+// import { Container } from './styles';
+import Pokeball from '../../assets/patterns/pokeball.svg';
 
 export default function Home() {
+  const color = '#aaa';
   return (
-    <Container><Text>Home</Text></Container>
+    <View>
+      <Pokeball
+        fill={color}
+
+        width={320}
+        height={320}
+      />
+    </View>
   );
 }
